93871ebb89869b80244401ef5906fc50f8642ba9
Alessandrasvitor <alessandrasilvav@gmail.com>
2019-12-30 10:54:37 -0300

    Commit do início do back

---
+++ gestao-pessoal-api/src/test/java/br/com/sansyro/sgp/api/GestaoPessoalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sansyro.sgp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPessoalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
